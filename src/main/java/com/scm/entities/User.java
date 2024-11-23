ae426ca0a4abcf35ca15dba27ebe42bd23b8a5e4
perwejalam03 <perwejalam03@gmail.com>
2024-11-23 08:54:58 +0530

    user mapped with contacts

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,10 +1,15 @@
 package com.scm.entities;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,10 +37,16 @@ public class User {
     private String about;
     @Column(length = 1000)
     private String profilePic;
-    private Integer phoneNumber;
-    private int enabled=0;
-    private int emailVerified=0;
-    private int phoneVerified=0;
+    private long phoneNumber;
+    private boolean enabled = false;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
     private Providers provider = Providers.SELF;
     private String providerUserId;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+
+    
 }
