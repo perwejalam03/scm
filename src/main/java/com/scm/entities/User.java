d3db31d2b66b3dc0caf4d66d49fb63d8a37e81c2
perwejalam03 <perwejalam03@gmail.com>
2024-11-25 08:51:28 +0530

    changes in user entity

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -8,6 +8,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -27,6 +29,7 @@ import lombok.Setter;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger userId;
     @Column(name = "user_name", nullable = false)
     private String name;
