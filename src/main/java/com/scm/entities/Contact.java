3ead1c382fdae67b99a756f00215a8d5bbabc7f2
perwejalam03 <perwejalam03@gmail.com>
2024-11-25 08:50:33 +0530

    contact added

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -9,6 +9,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -17,6 +19,7 @@ import jakarta.persistence.OneToMany;
 public class Contact {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String phoneNumber;
